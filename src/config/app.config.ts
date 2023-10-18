2d5e5936748785bee362a7fbbf3019dae77f1623
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-10-18 09:29:55 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,27 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+  getQuoteUrl: string;
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Business Owner'],
+  customerRoles: ['Guest'],
+  tenantRoles: ['Business Owner', 'HR Manager', 'Payroll Administrator', 'Employee', 'Accountant'],
+  tenantName: 'Company',
+  applicationName: 'HR Information System',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: [
+    'Read user information',
+    'Read company information',
+    'Read employee information',
+    'Read vacation information',
+  ],
+  ownerAbilities: ['Manage company information', 'Manage employee data', 'Manage payroll', 'Manage vacation requests'],
+  getQuoteUrl: 'https://roq-wizzard-git-qa03-roqtech.vercel.app/proposal/c454d363-e8af-4906-832d-87512c509800',
+};
